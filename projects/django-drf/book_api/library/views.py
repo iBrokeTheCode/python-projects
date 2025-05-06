8f74cc6bdd0f7fd2a5c3c27bdfa34f36be93be26
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-05-06 14:44:50 -0500

    feat: Add pagination for endpoints

--- projects/django-drf/book_api/library/views.py
+++ projects/django-drf/book_api/library/views.py
@@ -1,9 +1,33 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
 
 from library.filters import BookFilter
 from library.models import Book
 from library.serializers import BookSerializer
+
+# ================================================================
+#                          PAGINATION
+# ================================================================
+
+
+class BookListPNPagination(PageNumberPagination):
+    page_size = 3
+    page_query_param = "page"
+    page_size_query_param = "size"
+    max_page_size = 5
+
+
+class BookListLOPagination(LimitOffsetPagination):
+    default_limit = 3
+    limit_query_param = "limit"
+    offset_query_param = "offset"
+    max_limit = 5
+
+
+# ================================================================
+#                              VIEWS
+# ================================================================
 
 
 class BookListCreateView(generics.ListCreateAPIView):
@@ -11,6 +35,7 @@ class BookListCreateView(generics.ListCreateAPIView):
     serializer_class = BookSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = BookFilter
+    pagination_class = BookListPNPagination
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
