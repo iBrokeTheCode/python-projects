ba967190cda5b61549301cc40e2289e3e2e84444
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-05-07 15:35:54 -0500

    chore: Configure drf-simplejwt in project according documentation

--- projects/django-drf/book_api/book_api/settings.py
+++ projects/django-drf/book_api/book_api/settings.py
@@ -131,4 +131,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
 }
